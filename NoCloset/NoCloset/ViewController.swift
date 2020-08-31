3a469cd86d4fc4c1ca21255c0b9b05896878ad98
Pedro Spínola <pedrospinola@hotmail.com>
2020-08-31 08:36:01 -0300

    Primeiro commit

---
+++ NoCloset/NoCloset/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  NoCloset
+//
+//  Created by Pedro Henrique Spínola de Assis on 31/08/20.
+//  Copyright © 2020 Pedro Spínola. All rights reserved.
+//
+
+import UIKit
+
+//ViewController onde estarão agrupados os looks salvos
+class telaSeusLooks: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+//ViewController onde o usuário vai poder salvar um look novo
+class telaNovoLook: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+//ViewController onde o usuário vai poder visualizar um look salvo
+class telaVerLook: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
