6527fe3bcfa58e6a340c1a8adc2d881463570230
Pedro Spínola <pedrospinola@hotmail.com>
2020-08-31 17:56:15 -0300

    Estrutura das tableviews de collectionviews

---
+++ NoCloset/NoCloset/TableViewLooksCell.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewLooksCell.swift
+//  NoCloset
+//
+//  Created by Pedro Henrique Spínola de Assis on 31/08/20.
+//  Copyright © 2020 Pedro Spínola. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TableViewLooksCell: UITableViewCell{
+    
+    @IBOutlet weak var sessionOfLookTitle: UILabel!
+    
+    @IBOutlet weak var CollectionViewLook: UICollectionView!
+}
